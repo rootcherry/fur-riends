f27db2a78195e4649393157955d0e89142f48aea
Chris N <hi.chrisn@gmail.com>
2024-08-15 17:40:07 -0300

    set view engine

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
-
 const port = 3000;
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
